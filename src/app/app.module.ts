8871fc23f44a8ff0b6ba00144f612b4e8afb4db0
andre.rezende <>
2018-07-23 18:59:07 -0300

    New Angular 6 and graphql test project.

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import  {MatToolbarModule, MatListModule} from '@angular/material';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserAnimationsModule, 
+    MatListModule,
+    MatToolbarModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
