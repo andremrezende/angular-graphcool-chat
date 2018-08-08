e9f38a8def585979bbaa54b6f5555e39a8a55624
andre.rezende <>
2018-08-08 08:56:36 -0300

    Mensagens de erros no login com email obrigatorio e senha.

--- src/app/login/components/login/login.component.ts
+++ src/app/login/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl  } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loginForm:FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.createForm();
   }
 
+  createForm():void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    });
+  }
+
+  onSubmit():void {
+    console.log(this.loginForm.value);
+  }
+
+  get email(): FormControl {return <FormControl>this.loginForm.get('email');}
+  get password(): FormControl {return <FormControl>this.loginForm.get('password');}
 }
