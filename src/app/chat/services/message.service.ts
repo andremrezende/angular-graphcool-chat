ca32e4cfca6c3506b0f296287338459708a915bb
Andre Rezende <homerezende@gmail.com>
2018-10-09 10:37:46 -0300

    Adicionado acesso cache ao apollo de mensagens

--- src/app/chat/services/message.service.ts
+++ src/app/chat/services/message.service.ts
@@ -5,6 +5,7 @@ import { Observable } from 'rxjs';
 import { GET_CHAT_MESSAGES_QUERY, AllMessagesQuery, CREATE_MESSAGE_MUTATION } from './message.graphql';
 import { map } from 'rxjs/operators';
 import { Variable } from '@angular/compiler/src/render3/r3_ast';
+import { DataProxy } from 'apollo-cache';
 
 @Injectable({
   providedIn: 'root'
@@ -23,7 +24,21 @@ export class MessageService {
   createMessage(message: { text: string, chatId: string, senderId: string }): Observable<Message> {
     return this.apollo.mutate({
       mutation: CREATE_MESSAGE_MUTATION,
-      variables: message
+      variables: message,
+      update: (store: DataProxy, { data: { createMessage } }) => {
+        const data = store.readQuery<AllMessagesQuery>({
+          query: GET_CHAT_MESSAGES_QUERY,
+          variables: { chatId: message.chatId }
+        });
+
+        data.allMessages = [...data.allMessages, createMessage];
+        store.writeQuery( {
+          query: GET_CHAT_MESSAGES_QUERY,
+          variables: {chatId: message.chatId},
+          data
+        });
+        
+      }
     }).pipe(
       map(res => res.data.createMessage)
     );
