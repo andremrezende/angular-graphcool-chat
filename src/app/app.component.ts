731483a66974352791219b0790cfa07b925998d5
andre.rezende <>
2018-12-04 08:18:51 -0200

    adicionando fetchpolicy na lista de mensagens

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,7 +26,7 @@ export class AppComponent implements OnInit {
       .pipe(take(1))
       .subscribe(null, error => {
         const message = this.errorService.getErrorMessage(error);
-        this.snackBar.open(`Unexpected error: ${message}`, "Done", {
+        this.snackBar.open(`Error: ${message}`, "Done", {
           duration: 5000,
           verticalPosition: "top"
         });
